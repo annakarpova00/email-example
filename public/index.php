06a4c80885c9a8d9ff210b2f23fc8abbec19ef80
Dmitry Karport <demetrio77@gmail.com>
2024-01-18 15:39:40 +0300

    remove unnecessary code

--- public/index.php
+++ public/index.php
@@ -3,29 +3,17 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Slim\Psr7\Request;
 use Src\Sender;
 use Slim\Factory\AppFactory;
-use Slim\Http\ServerRequest;
-use DI\Container;
-use Slim\Views\PhpRenderer;
 
 $app = AppFactory::create();
 
-$container = new Container();
-$container->set('renderer', function () {
-    return new PhpRenderer(__DIR__ . '/../templates');
-});
-$app = AppFactory::createFromContainer($container);
-$app->addErrorMiddleware(true, true, true);
-
-$app->post('/', function (ServerRequest $request, $response)  {
+$app->post('/', function (Request $request, $response)  {
     $data = $request->getParsedBody();
     $email = new Sender();
-    return $response->write($email->send($data) ? 'ok' : 'error');
-});
-
-$app->get('/', function ($request, $response) {
-    return $this->get('renderer')->render($response, 'show.phtml', []);
+    $response->getBody()->write($email->send($data) ? 'ok' : 'error');
+    return $response;
 });
 
 $app->run();
